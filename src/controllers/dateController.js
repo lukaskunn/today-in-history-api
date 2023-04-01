1143e69ed25db047382e4e303bfce8bc56a15f02
lukaskunn <lucassioliveira098@gmail.com>
2023-04-01 11:59:57 -0300

    fixed puppeteer and vercel setup

--- src/controllers/dateController.js
+++ src/controllers/dateController.js
@@ -1,11 +1,35 @@
-const puppeteer = require("puppeteer")
+// const puppeteer = require("puppeteer")
+
 const validateDay = require("../utils/validateDate.js")
 const validateLanguage = require("../utils/validateLanguage.js")
 const getStringParam = require("../utils/getStringParam.js")
 const getDataStructurePattern = require("../utils/patterns/dataStructurePattern.js")
+
+let puppeteer
+let chrome = {}
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    chrome = require('chrome-aws-lambda')
+    puppeteer = require('puppeteer-core')
+} else {
+    puppeteer = require('puppeteer')
+}
 class DateController {
 
     static getWikipediaData = async (req, res) => {
+        let options = {};
+
+        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+            options = {
+                args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+                ignoreDefaultArgs: ['--disable-extensions'],
+                defaultViewport: chrome.defaultViewport,
+                executablePath: await chrome.executablePath,
+                headless: true,
+                ignoreHTTPSErrors: true
+            };
+        }
+
         const { language } = req.params
         const day = req.params.day || new Date().getDate()
         const month = req.params.month || new Date().getMonth() + 1
@@ -24,76 +48,84 @@ class DateController {
                 [dataStructurePattern.acceptableValues[2]]: "deaths",
             }
 
-            const browser = await puppeteer.launch()
-            const page = await browser.newPage()
+            try {
+                const browser = await puppeteer.launch(options)
+                const page = await browser.newPage()
 
 
-            await page.goto(`https://${language}.wikipedia.org/wiki/${stringMonthDayURL}`, {
-                waitUntil: 'networkidle2'
-            })
-
-            await page.waitForSelector('div.mw-parser-output');
-
-            const returnedEvents = await page.$$eval('div.mw-parser-output > ul, div.mw-parser-output > h2', rows => {
-                return Array.from(rows, row => {
-                    const col = row.querySelectorAll('li, span.mw-headline')
-                    return Array.from(col, c => c.textContent.trim().replace(/['"]+/g, ''))
+                await page.goto(`https://${language}.wikipedia.org/wiki/${stringMonthDayURL}`, {
+                    waitUntil: 'networkidle2'
                 })
-            })
 
-            const eventsObj = {
-                historical_events: {
-                    title: "",
-                    events: []
-                },
-                births: {
-                    title: "",
-                    events: []
-                },
-                deaths: {
-                    title: "",
-                    events: []
-                }
-            }
+                await page.waitForSelector('div.mw-parser-output');
 
-            let currentState = "historical_events"
+                const returnedEvents = await page.$$eval('div.mw-parser-output > ul, div.mw-parser-output > h2', rows => {
+                    return Array.from(rows, row => {
+                        const col = row.querySelectorAll('li, span.mw-headline')
+                        return Array.from(col, c => c.textContent.trim().replace(/['"]+/g, ''))
+                    })
+                })
 
-            for (let i = 0; i < returnedEvents.length; i++) {
-                if (returnedEvents[i][0] == dataStructurePattern.finishTag) {
-                    break
-                } else {
-                    if (dataStructurePattern.acceptableValues.includes(returnedEvents[i][0])) {
-                        currentState = currentStateMarker[returnedEvents[i][0]]
-                        eventsObj[currentState].title = returnedEvents[i][0]
-                    } else {
-                        returnedEvents[i].map((uniqueEvent) => {
-                            if (uniqueEvent.split(charCodeSplit)[1] != undefined || uniqueEvent.split("\n")[1] != undefined) {
-                                let resultObject = {
-                                    year: "",
-                                    [currentState == "historical_events" ? "events" : "peoples"]: []
-                                }
-
-                                if (uniqueEvent.split(charCodeSplit)[1] != undefined) {
-                                    resultObject.year = uniqueEvent.split(charCodeSplit)[0].trim()
-                                    resultObject[currentState == "historical_events" ? "events" : "peoples"].push(uniqueEvent.split(charCodeSplit)[1].split("[")[0].trim())
-                                } else if (uniqueEvent.split("\n")[1] != undefined) {
-                                    resultObject.year = uniqueEvent.split("\n")[0].trim()
-                                    for (let k = 1; k < uniqueEvent.split("\n").length; k++) {
-                                        resultObject[currentState == "historical_events" ? "events" : "peoples"].push(uniqueEvent.split("\n")[k].split("[")[0].trim())
-                                    }
-                                }
-
-                                eventsObj[currentState].events.push(resultObject)
-                            }
-                        })
+                const eventsObj = {
+                    historical_events: {
+                        title: "",
+                        events: []
+                    },
+                    births: {
+                        title: "",
+                        events: []
+                    },
+                    deaths: {
+                        title: "",
+                        events: []
                     }
                 }
+
+                let currentState = "historical_events"
+
+                for (let i = 0; i < returnedEvents.length; i++) {
+                    if (returnedEvents[i][0] == dataStructurePattern.finishTag) {
+                        break
+                    } else {
+                        if (dataStructurePattern.acceptableValues.includes(returnedEvents[i][0])) {
+                            currentState = currentStateMarker[returnedEvents[i][0]]
+                            eventsObj[currentState].title = returnedEvents[i][0]
+                        } else {
+                            returnedEvents[i].map((uniqueEvent) => {
+                                if (uniqueEvent.split(charCodeSplit)[1] != undefined || uniqueEvent.split("\n")[1] != undefined) {
+                                    let resultObject = {
+                                        year: "",
+                                        [currentState == "historical_events" ? "events" : "peoples"]: []
+                                    }
+
+                                    if (uniqueEvent.split(charCodeSplit)[1] != undefined) {
+                                        resultObject.year = uniqueEvent.split(charCodeSplit)[0].trim()
+                                        resultObject[currentState == "historical_events" ? "events" : "peoples"].push(uniqueEvent.split(charCodeSplit)[1].split("[")[0].trim())
+                                    } else if (uniqueEvent.split("\n")[1] != undefined) {
+                                        resultObject.year = uniqueEvent.split("\n")[0].trim()
+                                        for (let k = 1; k < uniqueEvent.split("\n").length; k++) {
+                                            resultObject[currentState == "historical_events" ? "events" : "peoples"].push(uniqueEvent.split("\n")[k].split("[")[0].trim())
+                                        }
+                                    }
+
+                                    eventsObj[currentState].events.push(resultObject)
+                                }
+                            })
+                        }
+                    }
+                }
+
+                res.status(200).send({ content: eventsObj })
+                await browser.close()
+            } catch (err) {
+                console.log(err)
+                res.status(500).send({
+                    message: `Internal server error`,
+                    error: err
+                })
             }
 
 
-            res.status(200).send({ content: eventsObj })
-
-            await browser.close()
         } else {
             res.status(400).send({
                 message: `wrong url param, please check and send request again`, params: {
